ed9cb14880811e3c145543e7eb04664fe689b638
Paula Schuppe <107902047+Paulas12@users.noreply.github.com>
2022-11-12 00:22:44 -0300

    Add files via upload

---
+++ Ejercicio-Unidad2.js
@@ -0,0 +1,11 @@
+const nacimiento = new Date("11 octubre 1996");
+console.log(nacimiento);
+
+const libroFav= {
+    titulo: "El psicoanalista",
+    autor: "John Katzenbach",
+    fecha: 2002,
+    URL: "drive.google.com/drive/u/0/search?q=psicoanalista" 
+}
+
+const lista = ["Paula", 26, true, nacimiento, libroFav];
